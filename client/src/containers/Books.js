a55360154414584b7089b37c82917d142be5efe9
Matéo Sossah <msossah@fb.com>
2022-08-02 20:39:50 +0200

    tried original image jpeg

--- client/src/containers/Books.js
+++ client/src/containers/Books.js
@@ -27,7 +27,7 @@ import page8EN from "../img/page8-en.png";
 import page9EN from "../img/page9-en.png";
 import page10EN from "../img/page10-en.png";
 import safetyIcons from "../img/paymentSafety.png";
-import ebookImageFR from "../img/ebookImage-FR.png";
+import ebookImageFR from "../img/ebookImage-FR.jpeg";
 import ebookImageEN from "../img/ebookImage-EN.png";
 import paperImageFR from "../img/paperImage-fr.png";
 import paperImageEN from "../img/paperImage-en.png";
