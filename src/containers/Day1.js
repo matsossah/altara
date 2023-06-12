58d94c32294ec6a1d0ce08ac248978136fc966a0
Mateo  Sossah <mateo.sossah@gmail.com>
2023-06-12 16:46:12 +0200

    Navbar underline fixed

--- src/containers/Day1.js
+++ src/containers/Day1.js
@@ -1,26 +1,35 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Preload, OrbitControls } from "@react-three/drei";
-import Portal from "../components/Portal.js"
+import Portal from "../components/Portal.js";
 import { useTranslation } from "react-i18next";
+import NavigationMenu from "../components/NavigationMenu";
 
 const Day1 = () => {
   const { t } = useTranslation();
   return (
-    <Canvas frameloop="demand" camera={{ position: [0, 0, 0.1] }}>
-      <OrbitControls
-        enableZoom={false}
-        enablePan={false}
-        enableDamping
-        dampingFactor={0.2}
-        autoRotate={false}
-        rotateSpeed={-0.5}
-      />
-      <Suspense fallback={null}>
-        <Preload all />
-        <Portal url={'/2294472375_24a3b8ef46_o.jpg'} position={[10, 0, -15]} label={t("cover.day2cta")} destination={"/day2"}/>
-      </Suspense>
-    </Canvas>
+    <>
+      <NavigationMenu />
+      <Canvas frameloop="demand" camera={{ position: [0, 0, 0.1] }}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          enableDamping
+          dampingFactor={0.2}
+          autoRotate={false}
+          rotateSpeed={-0.5}
+        />
+        <Suspense fallback={null}>
+          <Preload all />
+          <Portal
+            url={"/2294472375_24a3b8ef46_o.jpg"}
+            position={[10, 0, -15]}
+            label={t("cover.day2cta")}
+            destination={"/day2"}
+          />
+        </Suspense>
+      </Canvas>
+    </>
   );
 };
 
--- src/containers/Day2.js
+++ src/containers/Day2.js
@@ -1,26 +1,35 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Preload, OrbitControls } from "@react-three/drei";
-import Portal from "../components/Portal.js"
+import Portal from "../components/Portal.js";
 import { useTranslation } from "react-i18next";
+import NavigationMenu from "../components/NavigationMenu";
 
 const Day2 = () => {
   const { t } = useTranslation();
   return (
-    <Canvas frameloop="demand" camera={{ position: [0, 0, 0.1] }}>
-      <OrbitControls
-        enableZoom={false}
-        enablePan={false}
-        enableDamping
-        dampingFactor={0.2}
-        autoRotate={false}
-        rotateSpeed={-0.5}
-      />
-      <Suspense fallback={null}>
-        <Preload all />
-        <Portal url={'/Photosphere1.jpg'} position={[15, 0, 0]} label={t("cover.day3cta")} destination={"/day3"}/>
-      </Suspense>
-    </Canvas>
+    <>
+      <NavigationMenu />
+      <Canvas frameloop="demand" camera={{ position: [0, 0, 0.1] }}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          enableDamping
+          dampingFactor={0.2}
+          autoRotate={false}
+          rotateSpeed={-0.5}
+        />
+        <Suspense fallback={null}>
+          <Preload all />
+          <Portal
+            url={"/Photosphere1.jpg"}
+            position={[15, 0, 0]}
+            label={t("cover.day3cta")}
+            destination={"/day3"}
+          />
+        </Suspense>
+      </Canvas>
+    </>
   );
 };
 
